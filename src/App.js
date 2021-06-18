e59201c3ffa80c6a7dbab0c2c9cff76324682ec3
Aiste Tamosauskiene <aistetamosauskiene@gmail.com>
2021-06-18 13:02:09 +0300

    initial commit. Next step will be storing list to localStorage.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ToDoList from "./ToDoList";
+import ToDoForm from './ToDoForm';
+
+import './app.css';
 
 function App() {
+  
+  const [ todos, setTodos ] = useState([]);
+
+  const handleDelete = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+    setTodos(newTodos);
+  }
+
+  const addTask = (userInput ) => {
+    let copy = [...todos];
+    copy = [...copy, { id: todos.length + 1, task: userInput}];
+    setTodos(copy);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ToDoForm addTask={addTask}/>
+      <ToDoList todos={todos} handleDelete={handleDelete}/>
     </div>
   );
 }
