c2d2225eeec971fffa8fff51a2d209b29f8663ec
Aiste Tamosauskiene <aistetamosauskiene@gmail.com>
2021-06-22 11:57:24 +0300

    created functionality to store todo list tasks to a local storage, fixed styles to be more responsive to screen sizes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ToDoList from "./ToDoList";
 import ToDoForm from './ToDoForm';
 
 import './app.css';
 
 function App() {
-  
+
   const [ todos, setTodos ] = useState([]);
 
   const handleDelete = (id) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
-    setTodos(newTodos);
   }
 
   const addTask = (userInput ) => {
     let copy = [...todos];
-    copy = [...copy, { id: todos.length + 1, task: userInput}];
+    copy = [...copy, { id: userInput, task: userInput}];
     setTodos(copy);
   }
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('to_do_list'));
+    if (tasks) {
+      setTodos(tasks);
+    }
+  }, []);
+
+useEffect(() => {
+    localStorage.setItem('to_do_list', JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="app">
